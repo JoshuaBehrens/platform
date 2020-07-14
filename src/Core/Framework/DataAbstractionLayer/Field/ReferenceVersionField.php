9bc9efe9623bc18c5ca4c657c6aa77d6895029e9
Joshua Behrens <behrens@heptacom.de>
2020-07-15 01:12:26 +0200

    Add information to reference version field to know what field is referred to

--- src/Core/Framework/DataAbstractionLayer/Field/ReferenceVersionField.php
+++ src/Core/Framework/DataAbstractionLayer/Field/ReferenceVersionField.php
@@ -19,12 +19,23 @@ class ReferenceVersionField extends FkField
      */
     protected $versionReferenceDefinition;
 
-    public function __construct(string $definition, ?string $storageName = null)
+    /**
+     * @var string|null
+     */
+    protected $compoundIdStorageName;
+
+    /**
+     * @var string|null
+     */
+    protected $compoundIdPropertyName;
+
+    public function __construct(string $definition, ?string $storageName = null, ?string $compoundIdStorageName = null)
     {
         parent::__construct('', '', VersionDefinition::class);
 
         $this->versionReferenceClass = $definition;
         $this->storageName = $storageName;
+        $this->compoundIdStorageName = $compoundIdStorageName;
     }
 
     public function compile(DefinitionInstanceRegistry $registry): void
@@ -38,18 +49,38 @@ class ReferenceVersionField extends FkField
         $this->versionReferenceDefinition = $registry->get($this->versionReferenceClass);
         $entity = $this->versionReferenceDefinition->getEntityName();
         $storageName = $this->storageName ?? $entity . '_version_id';
+        $compoundIdStorageName = $this->compoundIdStorageName ?? $entity . '_id';
 
-        $propertyName = explode('_', $storageName);
-        $propertyName = array_map('ucfirst', $propertyName);
-        $propertyName = lcfirst(implode($propertyName));
+        if (!is_null($this->storageName) && is_null($this->compoundIdStorageName)) {
+            $storageNamePos = strpos($this->storageName, '_version_id');
+
+            if ($storageNamePos === (strlen($this->storageName) - strlen('_version_id'))) {
+                $compoundIdStorageName = substr($this->storageName, 0, $storageNamePos) . '_id';
+            }
+        }
+
+        $propertyName = $this->convertStorageToPropertyName($storageName);
+        $compoundIdPropertyName = $this->convertStorageToPropertyName($compoundIdStorageName);
 
         $this->storageName = $storageName;
+        $this->compoundIdStorageName = $compoundIdStorageName;
         $this->propertyName = $propertyName;
+        $this->compoundIdPropertyName = $compoundIdPropertyName;
     }
 
     public function getStorageName(): string
     {
         return $this->storageName;
+    }
+
+    public function getCompoundIdStorageName(): ?string
+    {
+        return $this->compoundIdStorageName;
+    }
+
+    public function getCompoundIdPropertyName(): ?string
+    {
+        return $this->compoundIdPropertyName;
     }
 
     public function getVersionReferenceDefinition(): EntityDefinition
@@ -65,5 +96,13 @@ class ReferenceVersionField extends FkField
     protected function getSerializerClass(): string
     {
         return ReferenceVersionFieldSerializer::class;
+    }
+
+    private function convertStorageToPropertyName(string $storageName): string
+    {
+        $propertyName = explode('_', $storageName);
+        $propertyName = array_map('ucfirst', $propertyName);
+
+        return lcfirst(implode($propertyName));
     }
 }
